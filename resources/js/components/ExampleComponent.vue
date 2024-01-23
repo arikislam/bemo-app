1c4e22eb7386a2d24ce39e964c28b8939ce6cae2
S M Ariq Islam <arikislam321@gmail.com>
2024-01-24 01:23:53 +0600

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello from My Simple Component!</h1>
+    <p>This is a basic Vue component in a Laravel application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // Data properties go here
+    };
+  },
+  // Methods, computed properties, etc. go here
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+p {
+  color: green;
+}
+</style>
